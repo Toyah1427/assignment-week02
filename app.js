502ab346a4e1bd16f974e5422fe3d780b94644b9
toyah1427 <toyahdegroat1427@gmail.com>
2024-03-28 17:23:47 +0000

    js added

---
+++ app.js
@@ -0,0 +1,48 @@
+const pickleBtn = document.getElementById("pickleBtn");
+const upgradeBtn = document.getElementById("upgradeBtn");
+const pickleSpan = document.getElementById("pickleSpan");
+const cpsSpan = document.getElementById("cpsSpan");
+
+const stats = {
+    pickleCount: 0,
+    cps: 0,
+};
+
+const storageStats = JSON.parse(localStorage.getitem("stats"));
+
+if (storageStats !== null) {
+    stats.pickleCount = storageStats.pickleCount;
+    stats.cps = storageStats.cps;
+    updatePage();
+}
+
+function buyPickle() {
+    stats.pickleCount++;
+    updatePage();
+    updateStorage();
+}
+
+function buyUpgrade() {
+    stats.cps++;
+    stats.pickleCount -= 10;
+    updatePage();
+    updateStorage();
+}
+
+function updatePage() {
+    pickleSpan.textContent = stats.pickleCount;
+    cpsSpan.textContent = stats.cps;
+}
+
+function updateStorage() {
+    localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+pickleBtn.addEventListener("click", buyPickle);
+upgradeBtn.addEventListener("click", buyUpgrade);
+
+setInterval(function () {
+    stats.pickleCount += stats.cps;
+    updatePage();
+    updateStorage();
+}, 1000);
